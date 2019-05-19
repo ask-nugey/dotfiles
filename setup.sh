15efa95343d70be3e7b6901569775e2a2a5e3957
yuki-iwase <mischief.moi@gmail.com>
2019-05-19 15:52:51 +0900

    追加

---
+++ setup.sh
@@ -0,0 +1,3 @@
+ln -fs '/Users/YUKI/git/dotfiles/hyper/.hyper.js' '/Users/YUKI/.hyper.js'
+ln -fs '/Users/YUKI/git/dotfiles/git/.gitconfig' '/Users/YUKI/.gitconfig'
+ln -fs '/Users/YUKI/git/dotfiles/vscode/settings.json' '/Users/YUKI/Library/Application Support/Code/User/settings.json'
